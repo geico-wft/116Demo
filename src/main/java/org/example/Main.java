df806fe367b60a93a718fbf070cb2fab8e656c07
Theodore Balashov <theodore.balashov@revature.com>
2024-01-16 10:32:19 -0500

    hello world

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import io.javalin.Javalin;
+
+/**
+ * Java is a high-level, compiled, object-oriented programming language
+ * Compilation will be the process of turning our human-readable code into
+ * machine-readable machine code.
+ * The JDK performs compilation,
+ * JRE (Java Runtime Environment) contains all the tools, standard libraries
+ * needed to run the compiled bytecode. It also contains something called
+ * the JVM (Java Virtual Machine), the JVM allows the program to run in the exact
+ * same way, in every single environment, without encountering issues *
+ * (platform independence)
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+    }
+
+}
+
+/**
+ * Maven is a "build tool"/"dependency management tool" -
+ * it allows us to build our project (compile it with any necessary external tools
+ * that we define, according to any project settings that we define)
+ * and also to manage any external tools that we wish to use
+ * All these things are managed within the pom.xml file.
+ */
+/**
+ * Git is a version control tool, that allows us to manage & keep a history of
+ * different version of our project, where every version is a "commit".
+ * A git repository is just a folder where Git is keeping track of versioning.
+ */
